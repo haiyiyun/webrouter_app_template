fdfe33654fec34e0e2256cfa8388d377f2dbe114
leetaifook <lee@leetaifook.com>
2021-08-23 01:26:07 +0800

    update

--- internal/app/app1/service/base/config.go
+++ internal/app/app1/service/base/config.go
@@ -4,23 +4,23 @@ import (
 	"github.com/haiyiyun/config"
 )
 
-type MongodbCfg struct {
+type MongodbConfig struct {
 	MongoDNS          string `json:"mongo_dns"`
 	MongoDatabaseName string `json:"mongo_database_name"`
 }
 
-type CacheCfg struct {
+type CacheConfig struct {
 	CacheDefaultExpiration config.Duration `json:"cache_default_expiration"`
 	CacheCleanupInterval   config.Duration `json:"cache_cleanup_interval"`
 }
 
-type BaseCfg struct {
+type BaseConfig struct {
 	WebRouter         bool   `json:"web_router"`
 	WebRouterRootPath string `json:"web_router_root_path"`
 }
 
 type Config struct {
-	MongodbCfg
-	CacheCfg
-	BaseCfg
+	MongodbConfig
+	CacheConfig
+	BaseConfig
 }
